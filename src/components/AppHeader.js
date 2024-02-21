04198dcecfc07614538d9b75e07441b317623b78
Jamie Gracie <jamie@gracie.kiwi>
2024-02-21 15:40:54 +0300

    CHANGED: Formatted date from Pirate Weather

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -13,12 +13,34 @@ import {
   CNavItem,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
-import { cilMenu, cilSun } from '@coreui/icons'
+import { cilMenu } from '@coreui/icons'
 
 import { AppBreadcrumb } from './index'
 import { logo } from 'src/assets/brand/logo'
 import WindIndicator from './WindDirection'
 import WeatherDisplay from './WeatherDisplay'
+
+function formatTimestamp(timestamp) {
+  const date = new Date(timestamp * 1000) // Convert timestamp to milliseconds
+  const formatter = new Intl.DateTimeFormat('en-GB', {
+    year: 'numeric',
+    month: 'long',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    timeZone: 'UTC',
+  })
+
+  let formattedDate = formatter.format(date)
+
+  // Add ordinal suffix to day
+  const day = date.getUTCDate()
+  const suffix = ['th', 'st', 'nd', 'rd'][
+    day % 10 > 3 ? 0 : (((day % 100) - (day % 10) != 10) * day) % 10
+  ]
+
+  return formattedDate.replace(/(\d+)(th)/, `${day}${suffix}`)
+}
 
 const AppHeader = () => {
   const dispatch = useDispatch()
@@ -83,40 +105,50 @@ const AppHeader = () => {
       <CHeaderDivider />
       <CContainer fluid>
         <AppBreadcrumb />
-        <CHeaderNav className="d-none d-md-flex ms-auto">
-          <CNavItem>
-            <small className="text-body text-opacity-75">
-              Last updated: 21st February 2024 13:05
-            </small>
-          </CNavItem>
-        </CHeaderNav>
-        <CHeaderNav className="ms-auto d-md-none">
-          <CNavItem>
-            <WeatherDisplay
-              condition={data.currently.icon}
-              temperature={data.currently.temperature}
-              apparentTemperature={data.currently.apparentTemperature}
-            />
-          </CNavItem>
-          <li className="nav-item py-1">
-            <div className="vr h-100 mx-2 text-body text-opacity-75"></div>
-          </li>
-          <CNavItem>
-            <WindIndicator
-              direction={data.currently.windBearing}
-              speed={data.currently.windSpeed}
-            />
-          </CNavItem>
-          <li className="nav-item py-1">
-            <div className="vr h-100 mx-2 text-body text-opacity-75"></div>
-          </li>
-          <CNavItem>
-            <small className="text-body text-opacity-75">62{data.currently.humidity * 100}%</small>
-          </CNavItem>
-          <CNavItem>
-            <small className="text-body text-opacity-75">&nbsp;@ 21/02 13:05</small>
-          </CNavItem>
-        </CHeaderNav>
+        {loading && (
+          <CHeaderNav className="d-none d-md-flex ms-auto">
+            <CNavItem>
+              <small className="text-body text-opacity-75">Loading...</small>
+            </CNavItem>
+          </CHeaderNav>
+        )}
+        {data && (
+          <>
+            <CHeaderNav className="d-none d-md-flex ms-auto">
+              <CNavItem>
+                <small className="text-body text-opacity-75">
+                  Last updated: {formatTimestamp(data.currently.time)}
+                </small>
+              </CNavItem>
+            </CHeaderNav>
+            <CHeaderNav className="ms-auto d-md-none">
+              <CNavItem>
+                <WeatherDisplay
+                  condition={data.currently.icon}
+                  temperature={data.currently.temperature}
+                  apparentTemperature={data.currently.apparentTemperature}
+                />
+              </CNavItem>
+              <li className="nav-item py-1">
+                <div className="vr h-100 mx-2 text-body text-opacity-75"></div>
+              </li>
+              <CNavItem>
+                <WindIndicator
+                  direction={data.currently.windBearing}
+                  speed={data.currently.windSpeed}
+                />
+              </CNavItem>
+              <li className="nav-item py-1">
+                <div className="vr h-100 mx-2 text-body text-opacity-75"></div>
+              </li>
+              <CNavItem>
+                <small className="text-body text-opacity-75">
+                  {data.currently.humidity * 100}%
+                </small>
+              </CNavItem>
+            </CHeaderNav>
+          </>
+        )}
       </CContainer>
     </CHeader>
   )
